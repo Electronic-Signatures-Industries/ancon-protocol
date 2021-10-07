275172af952e8d0a2e910c7ad45ef2e4fc0ad85b
Rogelio Morrell <molekilla@gmail.com>
2021-10-07 18:55:16 -0500

    wip

--- x/anconprotocol/keeper/evm.go
+++ x/anconprotocol/keeper/evm.go
@@ -9,7 +9,18 @@ import (
 )
 
 var (
-	// CreateMetadata represent the signature of
+
+	// event _anconSendCrossmintRequest(
+	//     uint256 recipientChainId,
+	//     string fromTokenNft,
+	//     string toTokenNft,
+	//     string metadataHash,
+	//     string fromOwner,
+	//     string toOwner,
+	//     string permitHash,
+	//     string permitSignature
+	// );
+	SendCrossmintRequest abi.Event
 	// `event _anconCreateMetadata(
 	// address owner,
 	// string did,
@@ -23,10 +34,11 @@ var (
 )
 
 func init() {
-	CreateMetadata = CreateMetedataAbiEvent()
+	CreateMetadata = CreateMetadataAbiEvent()
+	SendCrossmintRequest = SendCrossmintRequestAbiEvent()
 }
 
-func CreateMetedataAbiEvent() abi.Event {
+func CreateMetadataAbiEvent() abi.Event {
 	addressType, _ := abi.NewType("address", "", nil)
 	//bytesAType, _ := abi.NewType("bytes[]", "", nil)
 	//bytesType, _ := abi.NewType("bytes", "", nil)
@@ -75,7 +87,7 @@ func CreateMetedataAbiEvent() abi.Event {
 
 func CreateMetadataAbi() abi.ABI {
 	event := make(map[string]abi.Event)
-	event["_anconCreateMetadata"] = CreateMetedataAbiEvent()
+	event["_anconCreateMetadata"] = CreateMetadataAbiEvent()
 
 	return abi.ABI{
 		Constructor: abi.Method{},
@@ -99,6 +111,139 @@ func NewCreateMetadataHook(anconprotocolKeeper Keeper) *CreateMetadataHook {
 func (h CreateMetadataHook) PostTxProcessing(ctx sdk.Context, txHash common.Hash, logs []*ethtypes.Log) error {
 	for _, log := range logs {
 		if len(log.Topics) == 0 || log.Topics[0] != CreateMetadata.ID {
+			continue
+		}
+		// if !ContractAllowed(log.Address) {
+		// 	// Check the contract whitelist to prevent accidental native call.
+		// 	continue
+		// }
+		unpacked, err := CreateMetadata.Inputs.Unpack(log.Data)
+		if err != nil {
+			//log.Warn("log signature matches but failed to decode")
+			continue
+		}
+
+		owner := sdk.AccAddress(unpacked[0].(common.Address).Bytes())
+		did := unpacked[1].(string)
+		name := unpacked[2].(string)
+		description := unpacked[3].(string)
+		image := unpacked[4].(string)
+		parent := unpacked[5].(string)
+		sources := unpacked[6].(string)
+		links := unpacked[7].(string)
+
+		msg := types.MsgMetadata{
+			Creator:                owner.String(),
+			Name:                   name,
+			Description:            description,
+			Image:                  image,
+			Owner:                  owner.String(),
+			Parent:                 parent,
+			Sources:                sources,
+			Links:                  links,
+			VerifiedCredentialRef:  "",
+			Did:                    did,
+			From:                   "",
+			EnableIpldForestAccess: false,
+			FactRef:                "",
+		}
+
+		err = msg.ValidateBasic()
+		if err != nil {
+			return err
+		}
+
+		lnk, _ := h.anconprotocolKeeper.AddMetadata(
+			ctx,
+			&msg,
+		)
+
+		ctx.EventManager().EmitEvents(sdk.Events{
+			sdk.NewEvent(
+				"AddMetadataEvent",
+				sdk.NewAttribute("TxHash", txHash.Hex()),
+				sdk.NewAttribute("Link", lnk),
+				sdk.NewAttribute("Sender", unpacked[0].(common.Address).Hex()),
+			),
+		})
+
+	}
+	return nil
+}
+
+func SendCrossmintRequestAbiEvent() abi.Event {
+	//	addressType, _ := abi.NewType("address", "", nil)
+	uint256Type, _ := abi.NewType("uint256", "", nil)
+	//bytesType, _ := abi.NewType("bytes", "", nil)
+	stringType, _ := abi.NewType("string", "", nil)
+	msg := abi.NewEvent(
+		"_anconSendCrossmintRequest",
+		"_anconSendCrossmintRequest",
+		false,
+		abi.Arguments{abi.Argument{
+			Name:    "recipientChainId",
+			Type:    uint256Type,
+			Indexed: false,
+		}, abi.Argument{
+			Name:    "fromTokenNft",
+			Type:    stringType,
+			Indexed: false,
+		}, abi.Argument{
+			Name:    "toTokenNft",
+			Type:    stringType,
+			Indexed: false,
+		}, abi.Argument{
+			Name:    "metadataHash",
+			Type:    stringType,
+			Indexed: false,
+		}, abi.Argument{
+			Name:    "fromOwner",
+			Type:    stringType,
+			Indexed: false,
+		}, abi.Argument{
+			Name:    "toOwner",
+			Type:    stringType,
+			Indexed: false,
+		}, abi.Argument{
+			Name:    "permitHash",
+			Type:    stringType,
+			Indexed: false,
+		}, abi.Argument{
+			Name:    "permitSignature",
+			Type:    stringType,
+			Indexed: false,
+		}},
+	)
+
+	return msg
+}
+
+func SendCrossmintRequestAbi() abi.ABI {
+	event := make(map[string]abi.Event)
+	event["_anconSendCrossmintRequest"] = SendCrossmintRequestAbiEvent()
+
+	return abi.ABI{
+		Constructor: abi.Method{},
+		Methods:     map[string]abi.Method{},
+		Events:      event,
+		Fallback:    abi.Method{},
+		Receive:     abi.Method{},
+	}
+}
+
+type SendCrossmintRequestHook struct {
+	anconprotocolKeeper Keeper
+}
+
+func NewSendCrossmintRequestHook(anconprotocolKeeper Keeper) *SendCrossmintRequestHook {
+	return &SendCrossmintRequestHook{
+		anconprotocolKeeper: anconprotocolKeeper,
+	}
+}
+
+func (h SendCrossmintRequestHook) PostTxProcessing(ctx sdk.Context, txHash common.Hash, logs []*ethtypes.Log) error {
+	for _, log := range logs {
+		if len(log.Topics) == 0 || log.Topics[0] != SendCrossmintRequest.ID {
 			continue
 		}
 		// if !ContractAllowed(log.Address) {
