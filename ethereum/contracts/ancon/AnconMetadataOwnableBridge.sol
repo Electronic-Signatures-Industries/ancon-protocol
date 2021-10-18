d4aff9c48eaa390a583f9b0a386c9895d4a69a8e
Kendall Kant <kantkendall@gmail.com>
2021-10-18 17:43:51 -0500

    changeOwner with proff & convert proof

--- ethereum/contracts/ancon/AnconMetadataOwnableBridge.sol
+++ ethereum/contracts/ancon/AnconMetadataOwnableBridge.sol
@@ -16,15 +16,48 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "./ICS23Verifier.sol";
 
 contract AnconMetadataOwnableBridge is ICS23Verifier {
-    using IBCExistenceProof for  IBCExistenceProof.Data;
+    using IBCExistenceProof for IBCExistenceProof.Data;
     event CrossMintCallbackReceived(
         address indexed newOwner,
         string indexed metadataHash,
         uint256 indexed tokenId
     );
 
-    constructor(
-    ) public {
+    constructor() public {}
+
+    function convertProof(IBCExistenceProof.Data memory testxp)
+        public
+        returns (bool)
+    {
+        LeafOp memory leafOp = LeafOp({
+            hash: HashOp(uint256(testxp.leaf.hash)),
+            prehash_key: HashOp(uint256(testxp.leaf.prehash_key)),
+            prehash_value: HashOp(uint256(testxp.leaf.prehash_value)),
+            len: LengthOp(uint256(testxp.leaf.length)),
+            valid: false,
+            prefix: bytes("")
+        });
+
+        // innerOpArr
+        InnerOp[] memory innerOpArr;
+
+        for (uint256 i = 0; i < testxp.path.length; i++) {
+            innerOpArr[i] = InnerOp({
+                valid: false,
+                hash: HashOp(uint256(testxp.leaf.hash)),
+                prefix: bytes(""),
+                suffix: bytes("")
+            });
+        }
+        ExistenceProof memory proof = ExistenceProof({
+            valid: false,
+            key: testxp.key,
+            value: testxp.value,
+            leaf: leafOp,
+            path: innerOpArr
+        });
+
+        return true;
     }
 
     /**
@@ -36,24 +69,18 @@ contract AnconMetadataOwnableBridge is ICS23Verifier {
         bytes memory pathBz,
         bytes memory value
     ) public returns (bool) {
-        IBCExistenceProof.Data storage testxp;
+        IBCExistenceProof.Data memory exProof;
         
-        testxp.decode(existenceProof);
-        LeafOp memory leafOp = LeafOp({
-            hash: testxp.leaf.hash,
-            prehash_key: testxp.leaf.prehash_key,
-            prehash_value: testxp.leaf.prehash_value,
-            len: testxp.leaf.length,
-            valid: false
-        });
+        exProof = IBCExistenceProof.decode(existenceProof);
 
-        ExistenceProof memory proof = ExistenceProof({
-            valid: false,
-            key: testxp.key,
-            value: testxp.value,
-            leaf: leafOp,
-            path: [InnerOp({})]
-        });
+        // IBCExistenceProof.Data memory memoryXp = IBCExistenceProof.Data({
+        //     key: testxp.key,
+        //     value: testxp.value,
+        //     leaf: testxp.leaf,
+        //     path: testxp.path
+        // });
+
+        convertProof(exProof);
 
         //proof.key = xp.
 
@@ -65,7 +92,7 @@ contract AnconMetadataOwnableBridge is ICS23Verifier {
         // _release
     }
 
-    function _lock(bytes memory data) internal returns (bool){
+    function _lock(bytes memory data) internal returns (bool) {
         // ERC721(tokenAddress)
         (
             string memory metadata,
